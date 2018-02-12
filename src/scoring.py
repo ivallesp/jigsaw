9e0ffa711caf25cfb7482c4a11cbd22b8a3a238e
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-02-12 13:49:09 +0100

    Add scoring.py module

---
+++ src/scoring.py
@@ -0,0 +1,44 @@
+import json
+
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
+
+from src.architecture import Architecture
+from src.common_paths import get_model_path, get_tensorboard_logs_path, get_observers_path
+from src.data_tools import load_scoring_data, get_batcher
+from src.tensorflow_utilities import start_tensorflow_session, get_summary_writer, TensorFlowSaver
+from src.architecture import Architecture
+from src.common_paths import get_model_path, get_tensorboard_logs_path
+
+
+project_id = "jigsaw"
+version_id = "v1"
+batch_size = 256
+config = json.load(open("settings.json"))
+
+df, allowed_symbols, cle = load_scoring_data()
+
+net = Architecture(class_cardinality=6, vocab_size=len(allowed_symbols) + 3, embedding_size=embedding_size,
+                   n_recurrent_units=n_recurrent_units, learning_rate=learning_rate, name=project_id)
+
+sess = start_tensorflow_session(device=str(config["device"]), memory_fraction=config["memory_fraction"])
+saver = TensorFlowSaver(get_model_path(project_id, version_id))
+saver.load(sess)
+
+batcher_scoring = get_batcher(df, batch_size, train=False)
+pbar = tqdm(batcher_scoring, unit=" btch", total=df_train.shape[0] // batch_size, ncols=75)
+outputs=[]
+ids=[]
+for i, (id, batch) in enumerate(pbar):
+    output = sess.run(net.core_model.output, feed_dict={net.ph.comment_in: batch,
+                                                         net.ph.is_train: False,
+                                                         net.ph.keep_prob: 1})
+    ids.append(id)
+    outputs.append(output)
+
+    c += 1
+
+pbar.close()
+
+
