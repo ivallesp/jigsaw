a999f07d25769afea84acce950fc38a322922bea
Ivan Valles Perez <ivan_valles_perez@mckinsey.com>
2018-02-14 09:15:41 +0100

    Fix dependency bug in metrics module

--- src/metrics.py
+++ src/metrics.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import precision_recall_fscore_support, roc_auc_score, roc_curve
 
-from src.utilities import flatten
+from src.general_utilities import flatten
 
 
 def apk(actual, predicted, k=10):
